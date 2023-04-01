804724b6b1ecb0145f1d5f2b7172b5b967cf3099
e.mercado <admin@wayalabs.com>
2023-04-01 10:11:21 -0400

    update .css and navbar.jsx

--- frontend/components/navigation/navbar.jsx
+++ frontend/components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://linktr.ee/wayalabs" target={"_blank"}>
+			<a href="./">
 				<img className={styles.macaw_logo} src="/loro-logo.png"></img>
 			</a>
 			<ConnectButton></ConnectButton>
