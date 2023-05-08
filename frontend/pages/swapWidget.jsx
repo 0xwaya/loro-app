e87751dc9b4224a4f8fb92ff64cd0bfb25099b99
e.mercado <admin@wayalabs.com>
2023-05-08 11:14:44 -0400

    .css changes

--- frontend/pages/swapWidget.jsx
+++ frontend/pages/swapWidget.jsx
@@ -1,11 +1,12 @@
 import { React } from 'react';
 import Head from 'next/head';
+import styles from '../styles/SwapWidget.module.css';
 import { SwapWidget } from '@uniswap/widgets'
 import '@uniswap/widgets/fonts.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head />
       <main className="Uniswap">
         <SwapWidget> </SwapWidget>
