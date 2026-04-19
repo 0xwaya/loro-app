9691d1c32b7bc4e727365e79e64de9233a848d97
e mercado <admin@wayalabs.com>
2026-04-19 15:15:32 -0400

    Prevent minter page crash when NFT contract env var is missing/invalid

--- frontend/components/TokensBalanceDisplay.jsx
+++ frontend/components/TokensBalanceDisplay.jsx
@@ -1,42 +1,40 @@
-import { useState, useEffect } from "react";
-import { useAccount } from "wagmi";
-import styles from "../styles/Balance.module.css";
-import Image from "next/image";
+import { useState, useEffect, useCallback } from 'react';
+import { useAccount } from 'wagmi';
+import styles from '../styles/Balance.module.css';
+import Image from 'next/image';
 
-// Define TokensBalancePanel component
 export default function TokensBalancePanel({ walletAddress, chain }) {
-  // Define state variables using the useState hook
   const [tokensBalance, setTokensBalance] = useState();
   const [isLoading, setIsloading] = useState(false);
   const [myAddress, setMyAddress] = useState();
 
-  // Get current address and connection status from useAccount hook
-  const { address, isConnected, isDisconnected } = useAccount();
+  const { address, isDisconnected } = useAccount();
 
-  // Define function to get token balances
-  const getBalance = async () => {
+  const getBalance = useCallback(async () => {
     setIsloading(true);
+
     if (!isDisconnected || walletAddress) {
       try {
-        const fetchedTokensBalance = await fetch("/api/getTokensBalance", {
-          method: "POST",
+        const fetchedTokensBalance = await fetch('/api/getTokensBalance', {
+          method: 'POST',
           body: JSON.stringify({
             address: isDisconnected ? walletAddress : address,
-            chain: chain,
+            chain,
           }),
         }).then((res) => res.json());
+
         setTokensBalance(fetchedTokensBalance);
       } catch (e) {
         console.log(e);
       }
     }
-    setIsloading(false);
-  };
 
-  // Fetch token balances when myAddress changes
+    setIsloading(false);
+  }, [address, chain, isDisconnected, walletAddress]);
+
   useEffect(() => {
-    if (address?.length) getBalance();
-  }, [myAddress]);
+    if (myAddress?.length) getBalance();
+  }, [getBalance, myAddress]);
 
   useEffect(() => {
     if (walletAddress?.length) setMyAddress(walletAddress);
@@ -46,10 +44,8 @@ export default function TokensBalancePanel({ walletAddress, chain }) {
     if (address?.length) setMyAddress(address);
   }, [address]);
 
-  // If neither walletAddress nor address are provided, return "No address"
-  if (!walletAddress && isDisconnected) return "No address";
+  if (!walletAddress && isDisconnected) return 'No address';
 
-  // Render TokensBalancePanel component
   return (
     <div className={styles.token_panel_container}>
       <div className={styles.tokens_box}>
@@ -57,33 +53,33 @@ export default function TokensBalancePanel({ walletAddress, chain }) {
           {myAddress?.slice(0, 6)}...{myAddress?.slice(myAddress.length - 4)}
         </div>
         {isLoading
-          ? "Loading..."
+          ? 'Loading...'
           : tokensBalance?.length &&
-          tokensBalance?.map((token, i) => {
-            const convertedBalance = Math.round(token.balance * 100) / 100;
-            return (
-              <div key={i} className={styles.token_container}>
-                <div className={styles.token_name}>
-                  {token.logo ? (
-                    <Image
-                      className={styles.image_container}
-                      src="/token-logo.png"
-                      alt="Token logo"
-                      width={100}
-                      height={100}
-                    />
-                  ) : (
-                    <div className={styles.image_placeholder_container}></div>
-                  )}
-                  <div className={styles.coin_name}>{token.name}</div>
+            tokensBalance?.map((token, i) => {
+              const convertedBalance = Math.round(token.balance * 100) / 100;
+              return (
+                <div key={i} className={styles.token_container}>
+                  <div className={styles.token_name}>
+                    {token.logo ? (
+                      <Image
+                        className={styles.image_container}
+                        src='/token-logo.png'
+                        alt='Token logo'
+                        width={100}
+                        height={100}
+                      />
+                    ) : (
+                      <div className={styles.image_placeholder_container}></div>
+                    )}
+                    <div className={styles.coin_name}>{token.name}</div>
+                  </div>
+                  <div className={styles.token_info}>
+                    <div className={styles.price}>{convertedBalance}</div>
+                    <div className={styles.coin_symbol}>{token.symbol}</div>
+                  </div>
                 </div>
-                <div className={styles.token_info}>
-                  <div className={styles.price}>{convertedBalance}</div>
-                  <div className={styles.coin_symbol}>{token.symbol}</div>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
       </div>
     </div>
   );
